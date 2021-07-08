7ec46516989cef5fb7284d926026fa75df7c0e26
OleGramit <ole.gramit@fh-bielefeld.de>
2021-07-08 22:35:19 +0200

    Chage Node_Env

--- client/src/components/Env.js
+++ client/src/components/Env.js
@@ -3,6 +3,6 @@
 // }
 
 export default {
-    BACK_URL: process.env.NODE_ENV === 'production' ? "http://sgse2021.westeurope.cloudapp.azure.com/api/nachrichten:8181" : "http://localhost:3333"
+    BACK_URL: process.env.NODE_ENV === 'production' ? "https://sgse2021.westeurope.cloudapp.azure.com/messages-api" : "http://localhost:8181"
 }
 
