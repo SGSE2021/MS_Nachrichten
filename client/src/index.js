b8306c51d9843eab9aed85089687708ae7dda312
OleGramit <ole.gramit@fh-bielefeld.de>
2021-07-11 10:26:58 +0200

    Change to root

--- client/src/index.js
+++ client/src/index.js
@@ -4,4 +4,4 @@ import ReactDOM from 'react-dom';
 import Ilias2 from './components/Ilias2';
 import getLoggedUser from './components/helper/GetCurrentUser';
 
-ReactDOM.render(<Ilias2 loggedUser={getLoggedUser()} />, document.querySelector('#root'));
+ReactDOM.render(<Ilias2 loggedUser={getLoggedUser()} />, document.querySelector('root'));
